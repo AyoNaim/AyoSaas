a2c4e85162d21b4cad31af339a3655d72019090e
AyoNaim Mide <ayonaim101@gmail.com>
2024-08-20 03:55:43 +0000

    made changes

--- ayosaas/middleware.ts
+++ ayosaas/middleware.ts
@@ -1,6 +1,9 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { authMiddleware, clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+
+export default authMiddleware({
+  publicRoutes: ['/api/wbehooks/clerk']
+})
 
 export const config = {
   matcher: [
